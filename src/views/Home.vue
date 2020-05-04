685dbb3c37770f9bd5d98cb71acff78cb47f3496
Jyumi <justinlu75@gmail.com>
2020-05-03 23:55:26 -0400

    Finished first draft of portfolio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,60 @@
 <template>
   <div class="outer-container">
-    <div class="home-image">
-      <!-- <img src="../assets/ig-site-image/home.jpg" style="height:100%" /> -->
+    <div class="container portfolio-page">
+      <div class="row p-3 flex align-items-center">
+        <div class="col-12 col-md-6 col-lg-6">
+          <h1>Hi, I'm Justin.</h1>
+          <div class="p-2">I'm an aspiring Web Developer, specializing in full-stack development. I also like to take picutres from time to time. Checkout my work and feel free to get in touch if you want to work wtih me.</div>
+          <b-button to="/contact">contact me</b-button>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-6">
+        <img src="../assets/ig-site-image/headshot.jpg" style="width: 100%" />
+        </div>
+      </div>
+
+      <div class="row p-3">
+        <div class="col-12 col-md-6 col-lg-6">
+          <div class="portfolio-content">
+            <figure>
+              <img src="../assets/ig-site-image/watchme.jpg" alt />
+            </figure>
+
+            <div class="entry-content flex flex-column align-items-center justify-content-center">
+              <h3>
+                <a>For your viewing pleasure:</a>
+              </h3>
+
+              <ul class="flex flex-wrap justify-content-center">
+                <li>
+                  <a href="https://www.instagram.com/just_jlu/">Instagram</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-6">
+          <div class="portfolio-content">
+            <figure>
+              <img src="../assets/ig-site-image/home.jpg" alt />
+            </figure>
+
+            <div class="entry-content flex flex-column align-items-center justify-content-center">
+              <h3>
+                <a>For your viewing pleasure:</a>
+              </h3>
+
+              <ul class="flex flex-wrap justify-content-center">
+                <li>
+                  <a href="https://justjlu.pixieset.com/">PixieSet</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <h1>Justin Lu</h1>
-    <h3>Web designer /</h3>
-    <h3>photographer</h3>
   </div>
 </template>
 
